11ce94968c1b8f47b3438b2ebf83ad1b45155095
Marc Michael Maifarth <291030004+mmaifarth@users.noreply.github.com>
2026-07-12 16:19:38 +0200

    Updates

--- assets/index-DsZD-zcJ.js
+++ assets/index-DsZD-zcJ.js
@@ -58,7 +58,7 @@ Error generating stack: `+e.message+`
 
           .cover-seal {
               position: absolute;
-              top: -50%;
+              top: 60%;
               left: 50%;
               transform: translate(-50%, -50%);
               width: 100%;
